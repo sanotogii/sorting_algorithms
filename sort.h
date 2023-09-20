af67afb7a82481ced42a23953c67b4e4d5c624a2
sanotogii <nasaereh@gmail.com>
2023-09-20 15:02:29 -0400

    init

---
+++ sort.h
@@ -0,0 +1,21 @@
+#ifndef _SORT_
+#define _SORT_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+#endif
