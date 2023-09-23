c0c2195f6e270ad225dbfc940d8006d8c4cebf39
sanotogii <nasaereh@gmail.com>
2023-09-23 07:29:20 -0400

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,10 +5,12 @@
  * @array: Pointer to the array to be sorted.
  * @size: Size of the array.
  */
+typedef int bool;
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp;
+	bool flag = 0;
 
 	if (size <= 1)
 		return;
@@ -22,8 +24,11 @@ void bubble_sort(int *array, size_t size)
 				tmp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = tmp;
+				flag = 1;
 				print_array(array, size);
 			}
+			if (flag == 0)
+				break;
 		}
 	}
 }
